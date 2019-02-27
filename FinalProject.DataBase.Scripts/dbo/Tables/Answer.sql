6979360f1d3b4af9fc9c9b66e08a46ebfbcfffe2
Teodor26 <teodorbear26@gmail.com>
2019-02-28 00:29:06 +0200

    Changes.

--- FinalProject.DataBase.Scripts/dbo/Tables/Answer.sql
+++ FinalProject.DataBase.Scripts/dbo/Tables/Answer.sql
@@ -1,6 +1,9 @@
 ﻿CREATE TABLE [dbo].[Answer] (
-    [Id]   INT            IDENTITY (1, 1) NOT NULL,
-    [Name] NVARCHAR (MAX) NULL,
+    [Id]         INT            IDENTITY (1, 1) NOT NULL,
+    [QuestionId] INT            NOT NULL,
+    [Name]       NVARCHAR (MAX) NULL,
     CONSTRAINT [PK_Answer] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
+
+
 
--- FinalProject.DataBase.Scripts/dbo/Tables/Question.sql
+++ FinalProject.DataBase.Scripts/dbo/Tables/Question.sql
@@ -1,14 +1,17 @@
 ﻿CREATE TABLE [dbo].[Question] (
-    [Id]       INT            IDENTITY (1, 1) NOT NULL,
-    [TypeId]   INT            NULL,
-    [ModuleId] INT            NULL,
-    [ThemeId]  INT            NULL,
-    [Text]     NVARCHAR (MAX) NULL,
+    [Id]             INT            IDENTITY (1, 1) NOT NULL,
+    [QuestionTypeId] INT            NULL,
+    [ModuleId]       INT            NULL,
+    [ThemeId]        INT            NULL,
+    [TestId]         INT            NULL,
+    [Text]           NVARCHAR (MAX) NULL,
     CONSTRAINT [PK_Question] PRIMARY KEY CLUSTERED ([Id] ASC),
     CONSTRAINT [FK_Question_Module] FOREIGN KEY ([ModuleId]) REFERENCES [dbo].[Module] ([Id]),
-    CONSTRAINT [FK_Question_QuestionType] FOREIGN KEY ([TypeId]) REFERENCES [dbo].[QuestionType] ([Id]),
+    CONSTRAINT [FK_Question_QuestionType] FOREIGN KEY ([QuestionTypeId]) REFERENCES [dbo].[QuestionType] ([Id]),
     CONSTRAINT [FK_Question_Theme] FOREIGN KEY ([ThemeId]) REFERENCES [dbo].[Theme] ([Id])
 );
+
+
 
 
 
