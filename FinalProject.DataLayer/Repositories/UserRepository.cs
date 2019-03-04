2112c028acc5981e0e6c6a1d811e36b2a35a3016
Teodor26 <teodorbear26@gmail.com>
2019-03-04 19:55:21 +0200

    Changes

---
+++ FinalProject.DataLayer/Repositories/UserRepository.cs
@@ -0,0 +1,20 @@
+﻿using FinalProject.DataLayer.DataModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalProject.DataLayer.Repositories
+{
+    public class UserRepository
+    {
+        public IEnumerable<User> GetAll()
+        {
+            using (var ctx = new FinalProjectDBEntities())
+            {
+                return ctx.Users.ToList();
+            }
+        }
+    }
+}
