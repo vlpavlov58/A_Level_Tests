3d1bdf8b985da9256fd7d088756fc6e78e98dd4a
Teodor26 <teodorbear26@gmail.com>
2019-04-21 21:15:44 +0300

    Changes in deleting

--- FinalProject/Controllers/StudentController.cs
+++ FinalProject/Controllers/StudentController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using FinalProject.BusinessLogic.Services;
+using FinalProject.EFLayer.DataModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -9,5 +11,32 @@ namespace FinalProject.Controllers
 {
     public class StudentController : ApiController
     {
+        private readonly IStudentService studentService;
+
+        public StudentController()
+        {
+            studentService = new StudentService();
+        }
+
+        [HttpGet]
+        public IHttpActionResult GetAll()
+        {
+            var courses = studentService.GetStudents();
+            return Ok();
+        }
+
+        [HttpPost]
+        public IHttpActionResult Add([FromBody] Student student)
+        {
+            studentService.Add(student);
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IHttpActionResult Delelte(int Id)
+        {
+            studentService.Delete(Id);
+            return Ok();
+        }
     }
 }
--- FinalProject/Controllers/TestController.cs
+++ FinalProject/Controllers/TestController.cs
@@ -1,8 +1,37 @@
-﻿using System.Web.Http;
+﻿using FinalProject.BusinessLogic.Services;
+using FinalProject.EFLayer.DataModels;
+using System.Web.Http;
 
 namespace FinalProject.Controllers
 {
     public class TestController : ApiController
     {
+        private readonly ITestService testService;
+
+        public TestController()
+        {
+            testService = new TestService();
+        }
+
+        [HttpGet]
+        public IHttpActionResult GetAll()
+        {
+            var courses = testService.GetTestList();
+            return Ok();
+        }
+
+        [HttpPost]
+        public IHttpActionResult Add([FromBody] Test test)
+        {
+            testService.Add(test);
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IHttpActionResult Delelte(int Id)
+        {
+            testService.Delete(Id);
+            return Ok();
+        }
     }
 }
